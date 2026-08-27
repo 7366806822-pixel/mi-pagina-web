0864e7674bb07f1a93d518bca4ea3099b38b881e
7366806822-pixel <7366806822@untrm.edu.pe>
2026-08-27 15:44:48 -0500

    Add thesis PWA source files

---
+++ tesis-juridica-untrm/sw.js
@@ -0,0 +1,8 @@
+const CACHE='tesis-untrm-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('tesis-untrm-')&&k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;}).catch(()=>caches.match(e.request)));
+});
